52bf2aac65004f71956d6649594a40fbe53828f1
Elea Roche <elroche@ensc.fr>
2022-10-13 09:19:35 +0200

    Création de du controleur API

--- Program.cs
+++ Program.cs
@@ -25,3 +25,14 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+using (var context = new TodoContext())
+{
+    /*Console.WriteLine("--- Inserting 3 Todos ---");
+    context.Add(new Todo { Task = "Réunion transpromo", Completed = false, Deadline = DateTime.Today });
+    context.Add(new Todo { Task = "Faire la vaisselle", Completed = true });
+    context.Add(new Todo { Task = "WEI", Completed = false, Deadline = DateTime.Parse("01-03-22") });
+
+    context.SaveChanges();*/
+
+}
